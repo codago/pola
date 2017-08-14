be7fb9101757cd5ab0d3e570d18c110730f0a93e
arry kurnia <arrykurnia@arrys-MacBook-Pro.local>
2017-08-14 15:23:21 +0700

    finish

---
+++ pola.js
@@ -0,0 +1,17 @@
+let pola = str => {
+  let strArray = str.split (" ")
+  for (let i = 0 ; i< 10 ; i++){
+    for (let j = 0; j<10 ; j++){
+    if (Number(strArray[0].replace("#",i)) * Number(strArray[2])=== Number(strArray[4].replace("#",j))){
+        return [i,j]
+      }
+
+      }
+    }
+
+  }
+
+console.log(pola("42#3 * 188 = 80#204"))
+// result [8,5]
+console.log(pola("8#61 * 895 = 78410#5"))
+// result [7,9]
